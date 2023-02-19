ce7377e2ea38e908870f5f6244e12170899b9caf
Nathom78 <99360304+Nathom78@users.noreply.github.com>
2023-02-19 18:32:19 +0100

    partie 1 : print resultat

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
 # Résolvez_des_problèmes_en_utilisant_des_algorithmes_en_Python
 Projet 7 du parcours OpenClassrooms Développeur d'application - Python
+
+### Prérequis
+* Python est bien installé sur votre ordinateur
+* Git installé (conseillé)
+
+# INSTALLATION ( pour windows )
+
+Créer un dossier vide. Il contiendra le code complet du projet
+## 1. Installation du site
+
+Ouvrez un terminal:
+
+Depuis le dossier précédemment créé, clonez le repository du programme avec la commande :
+
+<pre><code>git clone https://github.com/Nathom78/JustStreamIt.git</code></pre>
+
+Ou utiliser [ce repository](https://github.com/Nathom78/JustStreamIt.git) en téléchargeant le zip.
+<br>
+
+
+## 2. Installer l'API
+Vous devez cloner le repository de l'API [ce repository](https://github.com/OpenClassrooms-Student-Center/OCMovies-API-EN-FR.git)
+Ensuite suivre les notes depuis le repository, pour decomptacter la base de données et lancer le serveur de l'API.
+
+## 3. Installer les paquets nécessaires aux projets ( Juste pour apporter des améliorations)
+
+<br>
+Taper la commande suivante : 
+<pre> pip install -r requirements.txt </pre>
+Pour vérifier, taper cette commande :
+<pre><code>pip list</code></pre>
+Et vous devriez avoir :
+<pre><code>Package         Version
+--------------- --------
+numpy           1.24.2
+pandas          1.5.3
+pip             23.0.1
+python-dateutil 2.8.2
+pytz            2022.7.1
+setuptools      67.2.0
+six             1.16.0
+</code></pre>
+
+## 4. Execution du logiciel
+
+Il ne reste plus qu'à lancer un fichier à partir d'un terminal
+
+
+
+
+
+## Technologies
+[![My Skills](https://skillicons.dev/icons?i=git,github,python&theme=dark)](https://skillicons.dev)
+
+
+
+
--- bruteforce.py
+++ bruteforce.py
@@ -1,7 +1,7 @@
-from pandas import read_csv as rf
+import pandas as pd
+import timeit  # pour calcul du temps execution
 
-
-# import timeit # pour calcul du temps d' execution
+MAX_EXPENDITURE = 500
 
 
 def read_file():
@@ -13,27 +13,27 @@ def read_file():
     def pourcent(x):
         return int(x.replace("%", "")) / 100
 
-    part_one = rf("data/Partie2_virgules - Copie.csv", names=["stock", "price", "profit"], header=0,
-                  converters={"profit": pourcent})
-    print(part_one)
+    part_one = pd.read_csv("data/Partie1_virgules - Copie.csv", names=["stock", "price", "profit"], header=0,
+                           converters={"profit": pourcent})
+
     return part_one
 
 
 def combinations(iterable, r):
     """
-
+    Return r length subsequences, of elements from the input iterable.
     :param iterable:
     :param r:
-    :return: "Return r length subsequences of elements from the input iterable."
+    :return:
     """
     # combinations('ABCD', 2) --> AB AC AD BC BD CD
     # combinations(range(4), 3) --> 012 013 023 123
-    pool = list(iterable)
+    pool = tuple(iterable)
     n = len(pool)
     if r > n:
         return
     indices = list(range(r))
-    yield list(pool[i] for i in indices)
+    yield tuple(pool[i] for i in indices)
     while True:
         for i in reversed(range(r)):
             if indices[i] != i + n - r:
@@ -43,12 +43,11 @@ def combinations(iterable, r):
         indices[i] += 1
         for j in range(i + 1, r):
             indices[j] = indices[j - 1] + 1
-        yield list(pool[i] for i in indices)
+        yield tuple(pool[i] for i in indices)
 
 
 def algorithme(array_data):
     """
-
     :return:
     """
 
@@ -64,35 +63,76 @@ def put_price(array_result, dataframe):
 
     def replace(string_actions):
         for row in range(len(dataframe)):
-            print(string_actions)
-            print(str(dataframe.loc[row, "stock"]))
             if str(string_actions) == str(dataframe.loc[row, "stock"]):
                 return dataframe.loc[row].to_numpy()
 
-    print(array_result)
-
-    # print(array_result_string)
     for tuple_actions in array_result:
-        combination_array = []
-        combination_array += map(replace, tuple_actions)
-        price_array += tuple(combination_array[i] for i in range(len(combination_array)))
+        combination_array = tuple(map(replace, tuple_actions))
+        price_array += (combination_array,)
 
-    # print(array_result_string)
-
-    # print(price_array)
     return price_array
+
+
+def filter_for_max(list_result):
+    list_filtered = []
+
+    def less_than(actions):
+        add_price = 0
+
+        if len(actions) > 1:
+            for action in actions:
+                add_price += action[1]
+        else:
+            add_price = actions[0][1]
+
+        return add_price < MAX_EXPENDITURE
+
+    for tuple_actions in list_result:
+        combination_array = tuple(filter(less_than, (tuple_actions,)))
+        list_filtered += combination_array
+
+    return list_filtered
+
+
+def best_profit(filtered_list):
+    def sum_profit(actions):
+        add_profit = 0
+        if len(actions) > 1:
+            for action in actions:
+                add_profit += action[3]
+        else:
+            add_profit = actions[0][3]
+        return add_profit
+
+    new_list = sorted(filtered_list, key=sum_profit, reverse=True)
+    best_combination = tuple(new_list[0])
+    return best_combination
 
 
 def main():
     dataframe = read_file()
-    array_data = dataframe["stock"].to_numpy()
-    # print(dataframe.to_numpy())
+    stock_data = dataframe["stock"].to_numpy()
+
+    dataframe["profit_amount"] = dataframe["price"] * dataframe["profit"]
+    dataframe["profit_amount"].round(decimals=2)
 
     # pour calcul du temps de l'algorithme
-    # test3 = timeit.Timer(algorithme)
-    # print(test3.timeit(10))
-    result = put_price(algorithme(array_data), dataframe)
-    print(result)
+    test3 = timeit.Timer(algorithme)
+    print(test3.timeit(10))
+
+    combinations_possible = algorithme(stock_data)
+    result = put_price(combinations_possible, dataframe)
+    list_filtered = filter_for_max(result)
+    best_combination = best_profit(list_filtered)
+
+    pd_best_combination = pd.DataFrame(best_combination, columns=["stock", "price", "profit", "profit_amount"])
+    row_total = {"stock": ["total"], "price": [pd_best_combination["price"].sum()],
+                 "profit_amount": [pd_best_combination["profit_amount"].sum()]}
+    df_row_total = pd.DataFrame(row_total)
+    sum_profit = [pd_best_combination, df_row_total]
+    total = pd.concat(sum_profit)
+
+    print("best_combination\n", total)
 
 
 if __name__ == '__main__':
